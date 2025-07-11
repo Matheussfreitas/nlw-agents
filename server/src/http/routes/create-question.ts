bbc87dbb84c91838626cef7f8cbd7e7ca324792a
Matheus Freitas <mgsoutodelira@gmail.com>
2025-07-11 15:46:40 -0300

    feat: integration gemini api

--- server/src/http/routes/create-question.ts
+++ server/src/http/routes/create-question.ts
@@ -1,7 +1,10 @@
+import { vector } from 'drizzle-orm/pg-core';
 import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod';
 import { db } from '../../db/connection.ts';
 import { schema } from '../../db/schema/index.ts';
 import z from 'zod/v4';
+import { generateAnswer, generateEmbeddings } from '../../services/gemini.ts';
+import { and, eq, sql } from 'drizzle-orm';
 
 export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
   app.post('/rooms/:roomId/questions', {
@@ -17,9 +20,36 @@ export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
     const { roomId } = request.params;
     const { question } = request.body;
 
+    const embeddings = await generateEmbeddings(question);
+
+    const embeddingsAsString = `[${embeddings.join(',')}]`;
+
+    const chunks = await db.select(
+      {
+        id: schema.audioChunks.id,
+        transcription: schema.audioChunks.transcription,
+        similarity: sql<Number>`1 - (${schema.audioChunks.embeddings} <-> ${embeddingsAsString}::vector)`
+      }
+    )
+    .from(schema.audioChunks)
+    .where(and(
+      eq(schema.audioChunks.roomId, roomId),
+      sql`1 - (${schema.audioChunks.embeddings} <-> ${embeddingsAsString}::vector) > 0.7`
+    ))
+    .orderBy(sql`${schema.audioChunks.embeddings} <-> ${embeddingsAsString}::vector`)
+    .limit(3)
+
+    let answer: string | null = null;
+
+    if (chunks.length > 0) {
+      const transcriptions = chunks.map(chunk => chunk.transcription);
+      answer = await generateAnswer(question, transcriptions);
+    }
+
     const result = await db.insert(schema.questions).values({
       roomId,
-      question
+      question,
+      answer,
     }).returning()
 
     const insertQuestion = result[0];
@@ -30,6 +60,7 @@ export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
 
     return reply.status(201).send({
       questionId: insertQuestion.id,
+      answer
     });
   })
 
